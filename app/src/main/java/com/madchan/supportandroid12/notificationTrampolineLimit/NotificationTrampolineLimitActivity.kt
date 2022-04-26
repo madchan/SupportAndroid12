a9211f199eeb6f98e7cdaa86069e623928cf251c
chenfeng <chenfeng@flatincbr.com>
2022-04-26 17:06:06 +0800

    通知跳转不生效问题

--- app/src/main/java/com/madchan/supportandroid12/notificationTrampolineLimit/NotificationTrampolineLimitActivity.kt
+++ app/src/main/java/com/madchan/supportandroid12/notificationTrampolineLimit/NotificationTrampolineLimitActivity.kt
@@ -7,6 +7,7 @@ import android.app.PendingIntent
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
+import android.os.Handler
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
@@ -22,51 +23,56 @@ class NotificationTrampolineLimitActivity : AppCompatActivity() {
     fun notificationToStartService(view: View) {
         moveTaskToBack(false)
 
-        val pendingIntent: PendingIntent =
-            Intent(this, NotificationTrampolineLimitService::class.java).let { notificationIntent ->
-                PendingIntent.getService(this, 0, notificationIntent, PendingIntent.FLAG_IMMUTABLE)
+        Handler(mainLooper).postDelayed({
+            val pendingIntent: PendingIntent =
+                Intent(this, NotificationTrampolineLimitService::class.java).let { notificationIntent ->
+                    PendingIntent.getService(this, 0, notificationIntent, PendingIntent.FLAG_IMMUTABLE)
+                }
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                val mChannel = NotificationChannel("CHANNEL_ID", "name", NotificationManager.IMPORTANCE_HIGH)
+                mChannel.description = "descriptionText"
+                val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+                notificationManager.createNotificationChannel(mChannel)
+
+                val notification: Notification = NotificationCompat.Builder(this, "CHANNEL_ID")
+                    .setContentTitle("notificationToStartService")
+                    .setContentText("ContentText")
+                    .setContentIntent(pendingIntent)
+                    .setSmallIcon(R.drawable.ic_launcher_foreground)
+                    .build()
+
+                notificationManager.notify(0, notification)
             }
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val mChannel = NotificationChannel("CHANNEL_ID", "name", NotificationManager.IMPORTANCE_HIGH)
-            mChannel.description = "descriptionText"
-            val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
-            notificationManager.createNotificationChannel(mChannel)
-
-            val notification: Notification = NotificationCompat.Builder(this, "CHANNEL_ID")
-                .setContentTitle("notificationToStartService")
-                .setContentText("ContentText")
-                .setContentIntent(pendingIntent)
-                .setSmallIcon(R.drawable.ic_launcher_foreground)
-                .build()
-
-            notificationManager.notify(0, notification)
-        }
+        }, 3000)
     }
 
     fun notificationToStartBroadcastReceiver(view: View) {
         moveTaskToBack(false)
 
-        val pendingIntent: PendingIntent =
-            Intent(this, NotificationTrampolineLimitReceiver::class.java).let { notificationIntent ->
-                PendingIntent.getBroadcast(this, 0, notificationIntent, PendingIntent.FLAG_IMMUTABLE)
+        Handler(mainLooper).postDelayed({
+            val pendingIntent: PendingIntent =
+                Intent(this, NotificationTrampolineLimitReceiver::class.java).let { notificationIntent ->
+                    PendingIntent.getBroadcast(this, 0, notificationIntent, PendingIntent.FLAG_IMMUTABLE)
+                }
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                val mChannel = NotificationChannel("CHANNEL_ID", "name", NotificationManager.IMPORTANCE_HIGH)
+                mChannel.description = "descriptionText"
+                val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+                notificationManager.createNotificationChannel(mChannel)
+
+                val notification: Notification = NotificationCompat.Builder(this, "CHANNEL_ID")
+                    .setContentTitle("notificationToStartBroadcastReceiver")
+                    .setContentText("ContentText")
+                    .setContentIntent(pendingIntent)
+                    .setSmallIcon(R.drawable.ic_launcher_foreground)
+                    .build()
+
+                notificationManager.notify(1, notification)
             }
+        }, 3000)
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val mChannel = NotificationChannel("CHANNEL_ID", "name", NotificationManager.IMPORTANCE_HIGH)
-            mChannel.description = "descriptionText"
-            val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
-            notificationManager.createNotificationChannel(mChannel)
-
-            val notification: Notification = NotificationCompat.Builder(this, "CHANNEL_ID")
-                .setContentTitle("notificationToStartBroadcastReceiver")
-                .setContentText("ContentText")
-                .setContentIntent(pendingIntent)
-                .setSmallIcon(R.drawable.ic_launcher_foreground)
-                .build()
-
-            notificationManager.notify(1, notification)
-        }
     }
 
 }
